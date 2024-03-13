e8f703885d6a5f1a49188469f26df3d2c206c9c6
Dom-Mac <domenicochad@gmail.com>
2024-03-13 11:58:30 +0100

    Minimal repro

---
+++ src/index.tsx
@@ -0,0 +1,76 @@
+import { serveStatic } from "@hono/node-server/serve-static"
+import { Button, Frog } from "frog"
+// import { neynar } from 'frog/hubs'
+
+export const app = new Frog({
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+app.use("/*", serveStatic({ root: "./public" }))
+
+app.frame("/", (c) => {
+  return c.res({
+    action: "/working",
+    image: (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          background: "#000",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          color: "#fff",
+          padding: "1rem",
+          fontSize: "2rem"
+        }}
+      >
+        Welcome!
+      </div>
+    ),
+    intents: [<Button>See working component</Button>]
+  })
+})
+
+app.frame("/working", (c) => {
+  return c.res({
+    action: "/issue",
+    image: <FrameContainer>It works!</FrameContainer>,
+    intents: [<Button>Trigger issue</Button>]
+  })
+})
+
+app.frame("/issue", (c) => {
+  return c.res({
+    action: "/",
+    image: <Issue />,
+    intents: [<Button>Nothing displayed - Go back</Button>]
+  })
+})
+
+function Issue() {
+  return <FrameContainer>Issue here!</FrameContainer>
+}
+
+function FrameContainer({ children }: { children?: JSX.Element | string }) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        background: "#000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        color: "#fff",
+        padding: "1rem",
+        fontSize: "2rem"
+      }}
+    >
+      {children}
+    </div>
+  )
+}
